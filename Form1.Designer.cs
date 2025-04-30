e0784b372cf29277f61427c7465a791ea4009564
Oswald moran <oswaldomorantt@gmail.com>
2025-04-30 01:25:09 -0600

    Commit CRUD gridForm

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PruebaTecnicaFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDepartamento = new System.Windows.Forms.Label();
+            this.lblPuesto = new System.Windows.Forms.Label();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDepartamento = new System.Windows.Forms.TextBox();
+            this.txtPuesto = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.MaskedTextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(61, 56);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblDepartamento
+            // 
+            this.lblDepartamento.AutoSize = true;
+            this.lblDepartamento.Location = new System.Drawing.Point(351, 56);
+            this.lblDepartamento.Name = "lblDepartamento";
+            this.lblDepartamento.Size = new System.Drawing.Size(74, 13);
+            this.lblDepartamento.TabIndex = 3;
+            this.lblDepartamento.Text = "Departamento";
+            // 
+            // lblPuesto
+            // 
+            this.lblPuesto.AutoSize = true;
+            this.lblPuesto.Location = new System.Drawing.Point(61, 117);
+            this.lblPuesto.Name = "lblPuesto";
+            this.lblPuesto.Size = new System.Drawing.Size(40, 13);
+            this.lblPuesto.TabIndex = 4;
+            this.lblPuesto.Text = "Puesto";
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Location = new System.Drawing.Point(351, 117);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(39, 13);
+            this.lblSalario.TabIndex = 5;
+            this.lblSalario.Text = "Salario";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(64, 75);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(241, 20);
+            this.txtNombre.TabIndex = 6;
+           // this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtDepartamento
+            // 
+            this.txtDepartamento.Location = new System.Drawing.Point(354, 75);
+            this.txtDepartamento.Name = "txtDepartamento";
+            this.txtDepartamento.Size = new System.Drawing.Size(241, 20);
+            this.txtDepartamento.TabIndex = 7;
+            //this.txtDepartamento.TextChanged += new System.EventHandler(this.txtDepartamento_TextChanged);
+            // 
+            // txtPuesto
+            // 
+            this.txtPuesto.Location = new System.Drawing.Point(64, 133);
+            this.txtPuesto.Name = "txtPuesto";
+            this.txtPuesto.Size = new System.Drawing.Size(241, 20);
+            this.txtPuesto.TabIndex = 8;
+            //this.txtPuesto.TextChanged += new System.EventHandler(this.txtPuesto_TextChanged);
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(354, 133);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(241, 20);
+            this.txtSalario.TabIndex = 10;
+            //this.txtSalario.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtSalario_MaskInputRejected);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(64, 206);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(93, 44);
+            this.btnNuevo.TabIndex = 11;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(212, 206);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(93, 45);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(354, 206);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(93, 46);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(502, 206);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(93, 46);
+            this.btnListar.TabIndex = 14;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 305);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(670, 202);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 507);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtPuesto);
+            this.Controls.Add(this.txtDepartamento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.lblPuesto);
+            this.Controls.Add(this.lblDepartamento);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Empleados";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDepartamento;
+        private System.Windows.Forms.Label lblPuesto;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDepartamento;
+        private System.Windows.Forms.TextBox txtPuesto;
+        private System.Windows.Forms.MaskedTextBox txtSalario;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Configuration;
+
+namespace PruebaTecnicaFinal
+{
+    public partial class Form1 : Form
+    {
+        int id = 0;
+        private string connectionString = ConfigurationManager.ConnectionStrings["PruebaTecnicaConnection"].ConnectionString;
+
+        //private string connectionString = "Server=DESKTOP-2P3UIN9;Database=PruebaTecnica;Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+            LlenarDataGridView("SP_ListarEmpleados", CommandType.StoredProcedure);
+        }
+
+        
+        private void LlenarDataGridView(string query, CommandType commandType)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.CommandType = commandType;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (id > 0)
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("SP_ActualizarEmpleado", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Departamento", txtDepartamento.Text);
+                        command.Parameters.AddWithValue("@Puesto", txtPuesto.Text);
+                        command.Parameters.AddWithValue("@Salario", txtSalario.Text);
+
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Empleado actualizado correctamente.");
+
+                        LimpiarCampos();
+                        LlenarDataGridView("SP_ListarEmpleados", CommandType.StoredProcedure); //Refresh
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleciona un empleado para actualizar");
+            }
+                
+        }
+
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (id > 0)
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("SP_EliminarEmpleado", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Empleado eliminado correctamente.");
+
+                        LimpiarCampos();
+                        LlenarDataGridView("SP_ListarEmpleados", CommandType.StoredProcedure);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se ha podido eliminar, intenta de nuevo");
+            }
+                
+        }
+
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Clear();
+            txtDepartamento.Clear();
+            txtPuesto.Clear();
+            txtSalario.Clear();
+        }
+
+        // cargar datos del empleado en Input
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) // seleccionar fila válida
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                txtNombre.Text = row.Cells["Nombre"].Value.ToString();
+                txtDepartamento.Text = row.Cells["Apellido"].Value.ToString();
+                txtPuesto.Text = row.Cells["Email"].Value.ToString();
+                txtSalario.Text = row.Cells["Telefono"].Value?.ToString();
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SP_InsertarEmpleado", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                    command.Parameters.AddWithValue("@Departamento", txtDepartamento.Text);
+                    command.Parameters.AddWithValue("@Puesto", txtPuesto.Text);
+                    command.Parameters.AddWithValue("@Salario", txtSalario.Text);
+
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Empleado insertado correctamente.");
+
+                    LimpiarCampos();
+
+                    LlenarDataGridView("SP_ListarEmpleados", CommandType.StoredProcedure); // Refresh DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+      
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            LlenarDataGridView("SP_ListarEmpleados", CommandType.StoredProcedure);
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            LlenarDataGridView("SP_ListarEmpleados", CommandType.StoredProcedure);
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                id = (int) selectedRow.Cells["ID"].Value;
+                txtNombre.Text = selectedRow.Cells["Nombre"].Value.ToString();
+                txtDepartamento.Text = selectedRow.Cells["Departamento"].Value.ToString();
+                txtPuesto.Text = selectedRow.Cells["Puesto"].Value.ToString();
+                txtSalario.Text = selectedRow.Cells["Salario"].Value.ToString();
+
+            }
+        }
+    }
+}
+
+
+
